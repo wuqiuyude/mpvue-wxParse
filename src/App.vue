7d598f8effaf9be24fcbbacc9e7f6662b32736e7
F-loat <945852046@qq.com>
2018-04-14 16:42:30 +0800

    更新依赖，优化显示效果

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,9 @@ export default {
 <style>
 .container {
   height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
   box-sizing: border-box;
   padding: 0 10px 5px;
+  overflow: auto;
 }
 
 .raw {
